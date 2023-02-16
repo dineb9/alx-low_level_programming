6c722c1890c32bf198401ce735db88f5beea637a
dineb9 <dineb90@gmail.com>
2023-02-16 04:56:22 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(void)
+/* This is main -function
+ *
+ * Return (Success)
+ * 
+ * */
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 } 
